9fd9d1781511fd93da30af85d1bea8dfb0d56dcb
niyi0002 <nilay.yilmaz0002@stud.hkr.se>
2021-06-20 21:26:45 +0200

    user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SALT = 10;
+var Schema = mongoose.Schema;
+var UserSchema = new Schema(
+  {
+    name: {type: String},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+  
+  }
+);
+
+// Virtual for tvshows's URL
+UserSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/user/login/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('User', UserSchema);
