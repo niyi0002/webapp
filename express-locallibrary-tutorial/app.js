720422e881b2b2e2e4194f4dc6f14ce5e3efb8a4
niyi0002 <nilay.yilmaz0002@stud.hkr.se>
2021-06-20 19:12:45 +0200

    Added files and changes required for deployment to heroku

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -4,19 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
-var app = express();
 
-//Set up mongoose connection
-var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://nilay:coolpassword@cluster0.lv52f.mongodb.net/tvshow_library?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,9 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter); 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -45,6 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
