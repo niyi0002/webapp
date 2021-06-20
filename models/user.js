834aec0e620d3afae5d8924593df0381c0f06e02
niyi0002 <nilay.yilmaz0002@stud.hkr.se>
2021-06-20 22:53:23 +0200

    bcrypt agaiin

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ var UserSchema = new Schema(
   {
     name: {type: String},
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    password: {},
   
   }
 );
